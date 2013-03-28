4d0f5ac39b1063261bb449cb9e37ca35842b2dab
mitermayer.reis@gmail.com <mitermayer.reis@gmail.com>
2013-03-29 00:31:13 +1100

    base class implemented

--- app/lib/base.js
+++ app/lib/base.js
@@ -16,9 +16,7 @@
         // Constructor
         this.init = function( obj ) {
 
-            var defaultEvents = ["update", "save", "sync", "validate", "destroy"];
-
-            element = obj.element;
+            var defaultEvents = ["update", "save", "sync", "validate"]; element = obj.element;
 
             // setup default events listeners
             for( var i=0, e=defaultEvents.lenght; i<e; i++ ) {
@@ -53,23 +51,90 @@
 
         };
 
-        // attach events
-        this.bind = function() {};
+        // attach event callback
+        this.bind = function( evnt, func ) {
+
+            if ( events[evnt] ) {
+
+                events[evnt].push( func );
+            }
+
+            return this;
+        };
 
         // remove events
-        this.unbind = function() {};
+        this.unbind = function( evnt, func ) {
+        
+            for( var e=0, v=events[evnt].length; e<v; e++ ) {
+
+                if( events[e] === func ) {
+
+                    events.splice(e, 1);
+                    break;
+                } 
+            }
+
+            return this;
+        };
 
         // update element value with custom element value
-        this.save = function() {};
+        this.save = function() {
+
+            var _val = this.element.value;
+        
+            this.element.value = value;
+
+            this.trigger("save", value);
+
+            // if an update has occured
+            if ( _val !== value ) {
+
+                this.trigger("update", value);
+            }
+        };
 
         // update custom element value with element value
-        this.sync = function() {};
+        this.sync = function() {
+
+            var _val = value;
+       
+            value = this.element.value;
+
+            this.trigger("sync", value);
+
+            // if an update has occured
+            if ( _val !== this.element.value ) {
+
+                this.trigger("update", value);
+            }
+        };
 
         // run custom element value over validators
-        this.validate = function() {};
+        this.validate = function() {
+            var ret = true;
+            
+            for(var v=0, l=validator.length; v<l; v++ ) {
+                
+                ret = validator[v]();
 
-        // detach events and destroy custom element
-        this.destroy = function() {};
+                if( !ret ) {
+                    break;
+                }
+            }
+
+            this.trigger("validate", ret);
+
+            return this;
+        };
+
+        // trigger custom event
+        this.trigger = function( evnt, data ) {
+            
+            for( var e=0, v=events[evnt].lenght; e<v; e++ ) {
+
+                events[evnt][e]( data );
+            }
+        };
 
         // call constructor
         this.init( obj );
