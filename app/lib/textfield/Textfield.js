0a59114f1400f4cf5bf9d539a9787d329e88f471
mitermayer.reis@gmail.com <mitermayer.reis@gmail.com>
2013-03-31 13:35:14 +1100

    refactored to abstract helper from class

--- app/lib/textfield/Textfield.js
+++ app/lib/textfield/Textfield.js
@@ -12,54 +12,30 @@
 
             return ('placeholder' in global.document.createElement('input'));
         })()
-    };
+    },
 
-    module.TextField = function( obj ) {
-
-       var instance = false;
-
-       if( !settings.placeholder_support ) {
-
-           var $el = $(obj.element),
-               color = $el.css("color"), 
-               placeholder = $el.attr("placeholder"),
-               opt = obj,
-               toggleColor,
-               clearText,
-               setDefaultText;
-
-           opt.validators = [
-                function( val ) {
-                    return val !== placeholder;
-                }
-           ];
-
-           instance = new APP.BaseField(obj);
-
-           toggleColor = function( state ) {
-
-                $el.css("color", (state ? color : settings.blur_color));
-           };
-
-           clearText = function() {
-
-               instance.update("").save();
-           };
-
-           setDefaultText = function() {
-
-               instance.update(placeholder).save();
-           };
-
-           instance.bind("validate", function( state ) {
-                toggleColor(state);
-           });
-
-           // sync element state
+    HELPER = {
+        toggleColor: function( $el, color, state ) {
+            $el.css("color", (state ? color : settings.blur_color));
+        },
+        clearText: function( instance ) {
+           instance.update("").save();
+        },
+        setDefaultText: function( instance, placeholder ) {
+           instance.update(placeholder).save();
+        },
+        addPlaceholder: function( instance, setDefaultText ) {
            if ( !instance.sync().validate() ) {
                setDefaultText();
            }
-
+        },
+        removePlaceholder: function( instance, clearText, toggleColor ) {
+           if ( !instance.sync().validate() ) {
+               clearText();
+               toggleColor(true);
+           }
+        },
+        attachEvents: function( instance, clearText, toggleColor, setDefaultText, $el ) {
            $el
            .focusin(function(){
                if ( !instance.sync().validate() ) {
@@ -77,7 +53,58 @@
                    clearText();
                }
            });
+        }
+    };
 
+    module.TextField = function( obj ) {
+
+       var instance = false;
+
+       if( !settings.placeholder_support ) {
+
+           var $el = $(obj.element),
+               color = $el.css("color"), 
+               placeholder = $el.attr("placeholder"),
+               opt = obj,
+               toggleColor,
+               clearText,
+               setDefaultText,
+               addPlaceholder,
+               removePlaceholder,
+               attachEvents;
+
+           opt.validators = [
+                function( val ) {
+                    return val !== placeholder;
+                }
+           ];
+
+           instance = new APP.BaseField(opt);
+
+
+           clearText      = function() { HELPER.clearText( instance ); };
+           toggleColor    = function(state) { HELPER.toggleColor( $el, color, state ); };
+           setDefaultText = function() { HELPER.setDefaultText( instance, placeholder ); };
+
+
+           removePlaceholder = function() {
+               HELPER.removePlaceholder( instance, clearText, toggleColor );
+           };
+           addPlaceholder = function() {
+               HELPER.addPlaceholder( instance, setDefaultText );
+           };
+           attachEvents = function() {
+               HELPER.attachEvents( instance, clearText, toggleColor, setDefaultText, $el );
+           };
+
+
+           instance.bind("validate", function( state ) {
+                toggleColor(state);
+           });
+
+
+           attachEvents();
+           addPlaceholder();
        }
 
        return instance;
