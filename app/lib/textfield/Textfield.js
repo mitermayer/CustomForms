7925239bce44f249dec58bebdcf3019bf7ec0a77
mitermayer reis <mitermayer.reis@gmail.com>
2013-04-05 17:55:36 +1100

    Refactored module textfield

--- app/lib/textfield/Textfield.js
+++ app/lib/textfield/Textfield.js
@@ -5,7 +5,6 @@
 
     var APP = global.app = global.app || {},
         module = APP.module = APP.module || {},
-        helper = APP.helper = APP.helper || {},
 
         settings = {
             active: true,
@@ -14,64 +13,8 @@
             {
                 return ('placeholder' in global.document.createElement('input'));
             })()
-        },
-
-        HELPER = helper.TextField = {
-            toggleColor: function($el, color, state)
-            {
-                $el.css("color", (state ? color : settings.blur_color));
-            },
-            clearText: function(instance)
-            {
-                instance.update("", true).save();
-            },
-            setDefaultText: function(instance, placeholder)
-            {
-                instance.update(placeholder, true).save();
-            },
-            addPlaceholder: function(instance, setDefaultText)
-            {
-                if (!instance.sync().validate())
-                {
-                    setDefaultText();
-                }
-            },
-            removePlaceholder: function(instance, clearText, toggleColor)
-            {
-                if (!instance.sync().validate())
-                {
-                    clearText();
-                    toggleColor(true);
-                }
-            },
-            attachEvents: function(instance, clearText, toggleColor,
-                setDefaultText, $el)
-            {
-                $el.focusin(function()
-                {
-                    if (!instance.sync().validate())
-                    {
-                        clearText();
-                        toggleColor(true);
-                    }
-                })
-                    .focusout(function()
-                {
-                    if (!instance.sync().validate())
-                    {
-                        setDefaultText();
-                    }
-                })
-                    .closest('form')
-                    .on("submit", function()
-                {
-                    if (!instance.sync().validate())
-                    {
-                        clearText();
-                    }
-                });
-            }
         };
+
 
     module.TextField = function(obj)
     {
@@ -85,13 +28,58 @@
                 color = $el.css("color"),
                 placeholder = $el.attr("placeholder"),
                 opt = obj,
-                toggleColor,
-                clearText,
-                setDefaultText,
-                addPlaceholder,
-                removePlaceholder,
-                attachEvents;
 
+                clearText = function()
+                {
+                    instance.update("", true).save();
+                },
+                toggleColor = function(state)
+                {
+                    $el.css("color", (state ? color : settings.blur_color));
+                },
+                setDefaultText = function()
+                {
+                    instance.update(placeholder, true).save();
+                },
+                validationFailProxy = function(func)
+                {
+                    if (!instance.sync().validate())
+                    {
+                        func();
+                    }
+                },
+                addPlaceholder = function()
+                {
+                    validationFailProxy(function()
+                    {
+                        setDefaultText();
+                    });
+                },
+                attachEvents = function()
+                {
+                    $el.focusin(function()
+                    {
+                        validationFailProxy(function()
+                        {
+                            clearText();
+
+                            // overwrite default invalid color
+                            toggleColor(true);
+                        });
+                    })
+                        .focusout(function()
+                    {
+                        addPlaceholder();
+                    })
+                        .closest('form')
+                        .on("submit", function()
+                    {
+                        validationFailProxy(function()
+                        {
+                            clearText();
+                        });
+                    });
+                };
 
             opt.validators = opt.validators || [];
 
@@ -102,41 +90,10 @@
 
             instance = new APP.BaseField(opt);
 
-
-            clearText = function()
-            {
-                HELPER.clearText(instance);
-            };
-            toggleColor = function(state)
-            {
-                HELPER.toggleColor($el, color, state);
-            };
-            setDefaultText = function()
-            {
-                HELPER.setDefaultText(instance, placeholder);
-            };
-
-
-            removePlaceholder = function()
-            {
-                HELPER.removePlaceholder(instance, clearText, toggleColor);
-            };
-            addPlaceholder = function()
-            {
-                HELPER.addPlaceholder(instance, setDefaultText);
-            };
-            attachEvents = function()
-            {
-                HELPER.attachEvents(instance, clearText, toggleColor,
-                    setDefaultText, $el);
-            };
-
-
             instance.bind("validate", function(state)
             {
                 toggleColor(state);
             });
-
 
             attachEvents();
             addPlaceholder();
