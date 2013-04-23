031bf49a469c0267096b2d7785ade2c7693b540d
mitermayer reis <mitermayer.reis@gmail.com>
2013-04-23 15:04:43 +1000

    refactored file module

--- app/lib/file/File.js
+++ app/lib/file/File.js
@@ -5,7 +5,9 @@
     var APP = global.app = global.app || {},
         module = APP.module = APP.module || {},
 
-        settings = {
+        DEFAULTS = {
+            active: true,
+            ready: function() {},
             // Thats the default 'size' for a button
             // it is used to address some issues on firefox to apply the correct size.
             'BUTTON_BROWSER_SIZE': 36,
@@ -48,19 +50,19 @@
         var $el = $(obj.element),
             $customEl,
             $customContainer,
-            opt = obj ? $.extend(true, {}, settings, obj) : settings,
-            _id = settings.classPrefix + ($el.attr('id') || $el.attr('name')),
-            _class = settings.classPrefix + 'file',
+            SETTINGS = obj ? $.extend(true, {}, DEFAULTS, obj) : DEFAULTS,
+            _id = DEFAULTS.classPrefix + ($el.attr('id') || $el.attr('name')),
+            _class = DEFAULTS.classPrefix + 'file',
             _containerClass = _class + '-container',
-            _callback = obj.init || function() {},
             _size = {
                 width: 0,
                 height: 0,
                 size: 0
             },
+
             getButtonSize = function(width) {
                 // Firefox needs to set size to button in order for it to work
-                return width - opt.BUTTON_BROWSER_SIZE;
+                return width - SETTINGS.BUTTON_BROWSER_SIZE;
             },
             getFileName = function() {
                 return $el.val().split('\\').pop();
@@ -77,29 +79,29 @@
                 });
             };
 
-        opt.validators = opt.validators || [];
+        SETTINGS.validators = SETTINGS.validators || [];
 
-        opt.init = function() {
+        SETTINGS.init = function() {
             // hide element
-            $el.css(settings.hideCss);
+            $el.css(DEFAULTS.hideCss);
 
             //// create custom element
-            $customContainer = $("<" + opt.containerEle + "/>");
+            $customContainer = $("<" + SETTINGS.containerEle + "/>");
 
             // setup attr and styles to container
             $customContainer.attr({
                 id: _id + '-container',
                 'class': _containerClass
-            }).css(opt.customContainerCss);
+            }).css(SETTINGS.customContainerCss);
 
             // create custom element
-            $customEl = $("<" + opt.customEle + "/>");
+            $customEl = $("<" + SETTINGS.customEle + "/>");
 
             // setup attr and styles to custom element
             $customEl.attr({
                 id: _id,
                 'class': _class
-            }).css(opt.customElCss);
+            }).css(SETTINGS.customElCss);
 
 
             // add container before element
@@ -117,21 +119,20 @@
             _size.size = getButtonSize(parseInt(_size.width, 10));
 
             // we than extend elCss with the dimensions and apply them to element.
-            $el.css($.extend({}, opt.elCss, _size));
+            $el.css($.extend({}, SETTINGS.elCss, _size));
 
-            _callback();
+            SETTINGS.ready();
         };
 
-        instance = new APP.BaseField(opt);
+        instance = new APP.BaseField(SETTINGS);
 
         instance.bind('validate', function() {
             var _selectedText = getFileName();
 
-            $customEl.html(_selectedText ? _selectedText : opt.holderTxt);
+            $customEl.html(_selectedText ? _selectedText : SETTINGS.holderTxt);
         });
 
         instance.validate();
-
         attachEvents();
 
         return instance;
